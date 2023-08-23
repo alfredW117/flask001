e7b5c234edc541d5384c3481914bb66244656294
alfredW117 <142611391+alfredW117@users.noreply.github.com>
2023-08-23 13:43:46 -0600

    Update database.py

--- src/config/database.py
+++ src/config/database.py
@@ -1,9 +1,6 @@
 import psycopg2
 def obtener_conexion():
-    conn = psycopg2.connect(host='ec2-54-211-177-159.compute-1.amazonaws.com
-', port=5432, database='deknsbnj96nntp
-', user='pteruwyudmayqr', password='57eee1504c786de6cc0e993a2dad48aca0dbdfbeb115b0d0e66c34dea1c16899
-')
+    conn = psycopg2.connect(host='ec2-54-211-177-159.compute-1.amazonaws.com', port=5432, database='deknsbnj96nntp', user='pteruwyudmayqr', password='57eee1504c786de6cc0e993a2dad48aca0dbdfbeb115b0d0e66c34dea1c16899')
     if conn is None:
         print('Error connecting')
     else:
